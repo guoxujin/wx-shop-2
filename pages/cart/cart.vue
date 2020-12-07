6dd9ac1d0889eeb034216163928d89d0203569eb
guoxujin <1755176085@qq.com>
2020-12-07 10:35:02 +0800

    完成了tabbar功能的开发

---
+++ pages/cart/cart.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="">
+	  111111111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		mywwwwww
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
